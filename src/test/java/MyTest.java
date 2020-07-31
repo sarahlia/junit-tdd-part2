022e605b00bdb52ebee8db48479465d4d536ab09
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-07-30 21:03:00 -0500

    added gitignore

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyTest {
+
+    @Test
+    public void testIfNameEquals() {
+        String school = "unc";
+        assertEquals("unc", school);
+    }
+
+}
+
