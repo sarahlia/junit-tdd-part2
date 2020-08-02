b3ab970be4dc1bc3de3caab3f5b608fe7cab1a4e
Sarah L Nainggolan <sarah.l.nainggolan@gmail.com>
2020-08-01 23:43:36 -0500

    refactored the code using Before annotation.

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,17 +1,23 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StudentTest {
 
+    Student sarah;
+
+    @Before
+    public void setup() {
+        sarah = new Student(1L, "sarah");
+    }
+
     @Test
     public void testIfIdWorks() {
-        Student sarah = new Student(1L, "sarah");
         assertEquals(1L, sarah.getId());
     }
 
     @Test
     public void testIfNameWorks() {
-        Student sarah = new Student(1L, "sarah");
         assertEquals("sarah", sarah.getName() );
 
     }
